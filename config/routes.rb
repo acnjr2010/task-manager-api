240de2897032748fb5fb49a2cd5ee78683f0551b
Junior <nog.junior84@gmail.com.br>
2018-12-27 00:58:28 -0200

    - crud users

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 require 'api_version_constraint'
 
 Rails.application.routes.draw do
-  devise_for :users
-  namespace :api, defaults: { format: :json } do
-    namespace :v1, path: '/', constraints: ApiVersionConstraint.new(version: 1, default: true) do
-
+  # devise_for :users
+  namespace :api, defaults: { format: :json }, path: '/' do
+    namespace :v0, path: '/', constraints: ApiVersionConstraint.new(version: 1, default: true) do
+      resources :users, only: [:show, :create, :update, :destroy]
     end
   end
 end
