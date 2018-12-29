de7512142e150dc5ee22fbc3be8a67a8189b9ae9
Junior <nog.junior84@gmail.com.br>
2018-12-29 15:32:36 -0200

    Adding auth token and #info to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  #attr_accessor :name
-  #validates_presence_of :name
+  validates_uniqueness_of :auth_token
+
+  def info
+    "#{email} - #{created_at}"
+  end
 end
