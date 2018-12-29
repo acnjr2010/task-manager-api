8f3fa48f16c19f8ecd6ddb9e2e182d1eb3be54d1
Junior <nog.junior84@gmail.com.br>
2018-12-29 19:52:46 -0200

    adding action create in task controller

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -10,4 +10,18 @@ class Api::V1::TasksController < ApplicationController
     task = current_user.tasks.find(params[id])
     render json: task, status: 200
   end
+
+  def create
+    task = current_user.tasks.build(task_params)
+
+    task.save
+
+    render json: task, status: 201
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :description, :deadline, :done)
+  end
 end
